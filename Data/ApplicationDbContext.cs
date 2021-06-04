a89b4abc814443cb08c2631f573727d5bfb65e00
Christian Afreh <55987185+ChristianAfreh@users.noreply.github.com>
2021-06-04 17:30:16 +0000

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Employee_Management_App.Models;
+
+namespace Employee_Management_App.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Employee_Management_App.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee_Management_App.Models
+{   
+    public enum Department
+    {
+        Administration, Marketing, HR, Finance, Software
+    }
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName{ get; set; }
+        public string Position { get; set; }
+        public Department Department { get; set; }
+        public double Salary { get; set; }
+        public DateTime DateEmployed { get; set; }
+        public DateTime LastUpdated { get; set; }
+
+
+    }
+}
